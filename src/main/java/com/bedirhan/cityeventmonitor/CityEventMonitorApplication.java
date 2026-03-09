bf0f115b29d20e0ae26b23db807e6fd4685d2a45
bedirhaneren <163941707+bedirhaneren@users.noreply.github.com>
2026-03-09 15:20:29 +0300

    First commit

---
+++ src/main/java/com/bedirhan/cityeventmonitor/CityEventMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.bedirhan.cityeventmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityEventMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityEventMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bedirhan/cityeventmonitor/CityEventMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bedirhan.cityeventmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityEventMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
